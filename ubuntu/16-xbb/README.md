b422137dfb8c2e815f2d7a3a3b8271900b5f9482
Liviu Ionescu <ilg@livius.net>
2019-12-22 21:30:20 +0200

    add ubuntu/16-xbb v1.1

---
+++ ubuntu/16-xbb/README.md
@@ -0,0 +1,32 @@
+
+## Build Docker images
+
+There are two scripts:
+
+- `arm64-build-v1.1.sh` -> `ilegeul/ubuntu:arm64-16.04-xbb-v1.1`
+- `armhf-build-v1.1.sh` -> `ilegeul/ubuntu:armhf-16.04-xbb-v1.1`
+
+```console
+$ bash ~/Downloads/xpack-build-box.git/ubuntu/16-updated/arm64-build-v1.1.sh
+$ bash ~/Downloads/xpack-build-box.git/ubuntu/16-updated/armhf-build-v1.1.sh
+
+$ docker images
+```
+
+## Test
+
+The test was performed on a macOS.
+
+```console
+$ docker run --interactive --tty ilegeul/ubuntu:arm64-16.04-xbb-v1.1
+$ docker run --interactive --tty ilegeul/ubuntu:armhf-16.04-xbb-v1.1
+```
+
+## Publish
+
+To publish, use:
+
+```console
+$ docker push "ilegeul/ubuntu:arm64-16.04-xbb-v1.1"
+$ docker push "ilegeul/ubuntu:armhf-16.04-xbb-v1.1"
+```
---
+++ ubuntu/16-xbb/input/build-script-v1.1.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+# Safety settings (see https://gist.github.com/ilg-ul/383869cbb01f61a51c4d).
+
+if [[ ! -z ${DEBUG} ]]
+then
+  set ${DEBUG} # Activate the expand mode if DEBUG is anything but empty.
+else
+  DEBUG=""
+fi
+
+set -o errexit # Exit if command failed.
+set -o pipefail # Exit if pipe failed.
+set -o nounset # Exit if variable not set.
+
+# Remove the initial space and instead use '\n'.
+IFS=$'\n\t'
+
+# -----------------------------------------------------------------------------
+# Identify the script location, to reach, for example, the helper scripts.
+
+script_path="$0"
+if [[ "${script_path}" != /* ]]
+then
+  # Make relative path absolute.
+  script_path="$(pwd)/$0"
+fi
+
+script_name="$(basename "${script_path}")"
+
+script_folder_path="$(dirname "${script_path}")"
+script_folder_name="$(basename "${script_folder_path}")"
+
+# =============================================================================
+
+env
+
+apt-get install -y software-properties-common
+add-apt-repository ppa:ubuntu-toolchain-r/test
+apt update
+
+apt install --yes \
+git \
+curl \
+make \
+pkg-config \
+m4 \
+gawk \
+autoconf automake \
+libtool libtool-bin \
+gettext \
+bison \
+texinfo \
+patchelf \
+dos2unix \
+flex \
+perl \
+cmake \
+python python3 \
+g++-7 
+
+apt install --yes \
+libpython-dev \
+libpython3-dev 
+
+apt install --yes \
+texlive \
+texlive-generic-recommended \
+texlive-extra-utils
+
+echo
+gcc-7 --version
