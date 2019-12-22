3bed200baf65b51a3fd572a35ec18bc90d571501
Liviu Ionescu <ilg@livius.net>
2019-12-22 20:04:44 +0200

    add ubuntu/16-updated

---
+++ ubuntu/16-updated/armhf-build-v1.1.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+# Safety settings (see https://gist.github.com/ilg-ul/383869cbb01f61a51c4d).
+
+if [[ ! -z ${DEBUG} ]]
+then
+  set ${DEBUG} # Activate the expand mode if DEBUG is anything but empty.
+else
+  DEBUG=""
+fi
+
+set -o errexit # Exit if command failed.
+set -o pipefail # Exit if pipe failed.
+set -o nounset # Exit if variable not set.
+
+# Remove the initial space and instead use '\n'.
+IFS=$'\n\t'
+
+# -----------------------------------------------------------------------------
+# Identify the script location, to reach, for example, the helper scripts.
+
+script_path="$0"
+if [[ "${script_path}" != /* ]]
+then
+  # Make relative path absolute.
+  script_path="$(pwd)/$0"
+fi
+
+script_name="$(basename "${script_path}")"
+
+script_folder_path="$(dirname "${script_path}")"
+script_folder_name="$(basename "${script_folder_path}")"
+
+# =============================================================================
+
+cd "${script_folder_path}"
+
+source "../../helper/common-functions-source.sh"
+
+# -----------------------------------------------------------------------------
+
+arch="armhf"
+tag="ilegeul/ubuntu:armhf-16.04-updated"
+
+echo 
+echo "Building Docker image..."
+docker build --tag "${tag}" -f "${arch}-Dockerfile" .
+
+echo 
+echo "Done."
+
+# -----------------------------------------------------------------------------
