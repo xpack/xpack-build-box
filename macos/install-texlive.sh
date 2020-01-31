c318b6c8360734f81938f6512e12ef0473891161
Liviu Ionescu <ilg@livius.net>
2020-01-31 17:12:23 +0200

    macos: update texlive

--- macos/install-texlive.sh
+++ macos/install-texlive.sh
@@ -33,110 +33,62 @@ script_folder_path="$(dirname "${script_path}")"
 script_folder_name="$(basename "${script_folder_path}")"
 
 # =============================================================================
+# This script installs a local instance of TeX Live (https://tug.org/texlive/).
+
+TL_EDITION_YEAR="2018"
+TL_EDITION_FOLDER_NAME="install-tl-20180414"
+# Schemes: basic (~80 packs), medium (~1000 packs), full (~3400)
+TL_SCHEME="basic"
+
+WORK_FOLDER_PATH="${HOME}/Work"
+
+# The place where files are downloaded.
+CACHE_FOLDER_PATH="${WORK_FOLDER_PATH}/cache"
+# The install destination folder.
+INSTALL_FOLDER_PATH="${HOME}/opt/texlive-x"
+
+# Make all tools choose gcc, not the old cc.
+export CC=gcc
+export CXX=g++
+
+# -----------------------------------------------------------------------------
+
+helper_folder_path="$(dirname "${script_folder_path}")/helper"
+
+source "${helper_folder_path}/common-functions-source.sh"
+source "${helper_folder_path}/common-texlive-functions-source.sh"
+
+# -----------------------------------------------------------------------------
+
+detect_host
+
+echo
+echo "$(uname) XBB TexLive ${TL_EDITION_YEAR} build script started..."
 
 echo
 echo "Checking if Xcode Command Line Tools are installed..."
 xcode-select -p
 
-# -----------------------------------------------------------------------------
-# This script installs a local instance of TeX Live (https://tug.org/texlive/).
-
-tl_edition="2018"
-tl_archive_name="install-tl-unx.tar.gz"
-tl_archive_path="${HOME}/Downloads/${tl_edition}-${tl_archive_name}"
-tl_folder="/tmp/install-tl"
-
-# tl_url="http://mirror.ctan.org/"
-# tl_repo_url="${tl_url}"/systems/texlive/tlnet
-# tl_archive_url="${tl_url}"/systems/texlive/tlnet/${tl_archive_name}
-
-# tl_url="ftp://tug.org/historic"
-tl_url="ftp://ftp.math.utah.edu/pub/tex/historic"
-tl_repo_url="${tl_url}/systems/texlive/${tl_edition}/tlnet-final"
-tl_archive_url="${tl_url}/systems/texlive/${tl_edition}/${tl_archive_name}"
-
-# The install destination folder.
-texlive_prefix="${HOME}/opt/texlive"
+function xbb_activate()
+{
+  :
+}
 
 # -----------------------------------------------------------------------------
 
-mkdir -p "${HOME}/tmp"
-cd "${HOME}/tmp"
+do_texlive "${TL_EDITION_YEAR}" "${TL_EDITION_FOLDER_NAME}" "${TL_SCHEME}"
 
-# Download the install tools.
+# -----------------------------------------------------------------------------
+
 echo
-echo "Downloading '${tl_archive_url}'..."
-curl --fail -L "${tl_archive_url}" -o "${tl_archive_path}"
+echo "You may want to ' chmod -R -w \"${INSTALL_FOLDER_PATH}\" '"
 
-rm -rf "${tl_folder}"
-mkdir -p "${tl_folder}"
-
-# Unpack the install tools.
 echo
-echo "Unpacking '${tl_archive_path}'..."
-tar x -v -C "${tl_folder}" --strip-components 1 -f "${tl_archive_path}"
+echo "$(uname) XBB TexLive ${TL_EDITION_YEAR} created in \"${INSTALL_FOLDER_PATH}\""
 
-# -----------------------------------------------------------------------------
-
-if [ -d "${texlive_prefix}" ]
+if [ "${HOST_UNAME}" == "Darwin" ]
 then
-  rm -rf "${texlive_prefix}.bak"
-  echo "Backing-up previous install..."
-  mv "${texlive_prefix}" "${texlive_prefix}.bak"
+  say done
 fi
-
-mkdir -p "${texlive_prefix}"
-
-# -----------------------------------------------------------------------------
-
-# Create the texlive.profile used to automate the install.
-# These definitions are specific to TeX Live 2016.
-tmp_profile="$(mktemp)"
-
-echo
-echo "Profile file '${tmp_profile}'"
-
-# Note: __EOF__ is not quoted to allow local substitutions.
-cat <<__EOF__ >> "${tmp_profile}"
-# texlive.profile
-TEXDIR ${texlive_prefix}
-TEXMFCONFIG ~/.texlive/texmf-config
-TEXMFHOME ~/texmf
-TEXMFLOCAL ${texlive_prefix}/texmf-local
-TEXMFSYSCONFIG ${texlive_prefix}/texmf-config
-TEXMFSYSVAR ${texlive_prefix}/texmf-var
-TEXMFVAR ~/.texlive/texmf-var
-
-option_doc 0
-option_src 0
-__EOF__
-
-# -----------------------------------------------------------------------------
-
-# https://www.tug.org/texlive/doc/install-tl.html
-
-export PATH="${texlive_prefix}/bin/x86_64-darwin:${PATH}"
-
-# Schmes: basic (~80 packs), medium (~1000 packs), full (~3400)
-echo
-echo "Running install-tl..."
-time "${tl_folder}/install-tl" \
--repository "${tl_repo_url}" \
--no-gui \
--lang en \
--profile "${tmp_profile}" \
--scheme medium
-
-tlmgr install collection-fontsrecommended
-
-# Keep no backups (not required, simply makes cache bigger)
-tlmgr option -- autobackup 0
-
-# -----------------------------------------------------------------------------
-
-rm "${tmp_profile}"
-
-echo
-echo "Done."
 
 # -----------------------------------------------------------------------------
