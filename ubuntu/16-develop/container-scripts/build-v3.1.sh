49a381946157ffaeb98ce205c0a47c0624fa1727
Liviu Ionescu <ilg@livius.net>
2020-02-02 19:23:44 +0200

    ubuntu/16-[updated|develop}: fix for 'universe' update bug
    - do not add universe to updated
    - add universe in develop after main installs

--- ubuntu/16-develop/container-scripts/build-v3.1.sh
+++ ubuntu/16-develop/container-scripts/build-v3.1.sh
@@ -35,6 +35,7 @@ script_folder_name="$(basename "${script_folder_path}")"
 # =============================================================================
 
 env
+unset TERM
 
 # -----------------------------------------------------------------------------
 
@@ -42,32 +43,102 @@ env
 
 apt-get install --yes \
 \
-build-essential \
-\
-gcc++ \
-make \
-automake \
-pkg-config \
-curl \
-xz-utils \
-zip \
-unzip \
-bzip2 \
-libtool \
-gettext \
-texinfo \
+autoconf automake \
 bison \
+bzip2 \
+cmake \
+curl \
+diffutils \
+file \
 flex \
-dos2unix \
+gawk \
+gcc++ \
+gettext \
+git \
+libc6-dev \
+libtool \
+libtool-bin \
+m4 \
+make \
 patch \
 perl \
-zlib1g-dev \
-file \
-diffutils \
-cmake \
-libudev-dev \
+pkg-config \
+python \
+python3 \
 tcl \
+time \
+unzip \
 wget \
+xz-utils \
+zip \
+zlib1g-dev \
+
+# For QEMU
+apt-get install --yes \
+libx11-dev \
+libxext-dev \
+mesa-common-dev
+
+# For QEMU & OpenOCD
+apt-get install --yes \
+libudev-dev
+
+if true
+then
+cat <<'__EOF__' >>"/etc/apt/sources.list"
+
+## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
+## team. Also, please note that software in universe WILL NOT receive any
+## review or updates from the Ubuntu security team.
+deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial universe
+# deb-src http://us.ports.ubuntu.com/ubuntu-ports/ xenial universe
+deb http://ports.ubuntu.com/ubuntu-ports xenial-security universe
+# deb-src http://ports.ubuntu.com/ubuntu-ports xenial-security universe
+
+deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial-updates universe
+# deb-src http://us.ports.ubuntu.com/ubuntu-ports/ xenial-updates universe
+
+deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial-backports universe 
+# deb-src http://us.ports.ubuntu.com/ubuntu-ports/ xenial-backports universe 
+__EOF__
+fi
+
+if false
+then
+cat <<'__EOF__' >>"/etc/apt/sources.list"
+
+## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu 
+## team, and may not be under a free licence. Please satisfy yourself as to 
+## your rights to use the software. Also, please note that software in 
+## multiverse WILL NOT receive any review or updates from the Ubuntu
+## security team.
+deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial multiverse
+# deb-src http://us.ports.ubuntu.com/ubuntu-ports/ xenial multiverse
+deb http://ports.ubuntu.com/ubuntu-ports xenial-security multiverse
+# deb-src http://ports.ubuntu.com/ubuntu-ports xenial-security multiverse
+
+deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial-updates multiverse
+# deb-src http://us.ports.ubuntu.com/ubuntu-ports/ xenial-updates multiverse
+
+deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial-backports multiverse
+# deb-src http://us.ports.ubuntu.com/ubuntu-ports/ xenial-backports multiverse
+__EOF__
+fi
+
+echo
+echo "The resulting /etc/apt/sources.list"
+cat "/etc/apt/sources.list" | egrep '^deb '
+echo "---"
+
+apt-get update 
+
+# From  (universe)
+apt-get install --yes \
+dos2unix \
+patchelf \
+texinfo \
+
+# -----------------------------------------------------------------------------
 
 apt-get clean
 apt-get autoclean
@@ -78,5 +149,26 @@ apt-get autoremove
 echo
 uname -a
 lsb_release -a
+
+autoconf --version
+bison --version
+cmake --version
+curl --version
+flex --version
+g++ --version
+gawk --version
+git --version
+m4 --version
+make --version
+patch --version
+perl --version
+pkg-config --version
+python --version
+python3 --version
+
+# -----------------------------------------------------------------------------
+
+echo
+echo "Container done."
 
 # -----------------------------------------------------------------------------
--- ubuntu/16-updated/container-scripts/build-v3.1.sh
+++ ubuntu/16-updated/container-scripts/build-v3.1.sh
@@ -35,6 +35,7 @@ script_folder_name="$(basename "${script_folder_path}")"
 # =============================================================================
 
 env
+unset TERM
 
 # -----------------------------------------------------------------------------
 
@@ -48,28 +49,47 @@ echo "---"
 # Copied from a system installed directly from updates.
 # Note: __EOF__ is quoted to prevent substitutions here.
 cat <<'__EOF__'  >"/etc/apt/sources.list"
-# deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial main restricted
-
-# deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial-updates main restricted
-# deb http://ports.ubuntu.com/ubuntu-ports xenial-security main restricted
-
+# https://help.ubuntu.com/community/Repositories/Ubuntu
 # See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to
 # newer versions of the distribution.
+
 deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial main restricted
 # deb-src http://us.ports.ubuntu.com/ubuntu-ports/ xenial main restricted
+deb http://ports.ubuntu.com/ubuntu-ports xenial-security main restricted
+# deb-src http://ports.ubuntu.com/ubuntu-ports xenial-security main restricted
 
 ## Major bug fix updates produced after the final release of the
 ## distribution.
 deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial-updates main restricted
 # deb-src http://us.ports.ubuntu.com/ubuntu-ports/ xenial-updates main restricted
 
+deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial-backports main restricted 
+# deb-src http://us.ports.ubuntu.com/ubuntu-ports/ xenial-backports main restricted 
+__EOF__
+
+if false
+then
+cat <<'__EOF__' >>"/etc/apt/sources.list"
+
 ## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
 ## team. Also, please note that software in universe WILL NOT receive any
 ## review or updates from the Ubuntu security team.
 deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial universe
 # deb-src http://us.ports.ubuntu.com/ubuntu-ports/ xenial universe
+deb http://ports.ubuntu.com/ubuntu-ports xenial-security universe
+# deb-src http://ports.ubuntu.com/ubuntu-ports xenial-security universe
+
 deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial-updates universe
 # deb-src http://us.ports.ubuntu.com/ubuntu-ports/ xenial-updates universe
+
+deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial-backports universe 
+# deb-src http://us.ports.ubuntu.com/ubuntu-ports/ xenial-backports universe 
+__EOF__
+fi
+
+if false
+then
+cat <<'__EOF__' >>"/etc/apt/sources.list"
 
 ## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu 
 ## team, and may not be under a free licence. Please satisfy yourself as to 
@@ -78,36 +98,54 @@ deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial-updates universe
 ## security team.
 deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial multiverse
 # deb-src http://us.ports.ubuntu.com/ubuntu-ports/ xenial multiverse
+deb http://ports.ubuntu.com/ubuntu-ports xenial-security multiverse
+# deb-src http://ports.ubuntu.com/ubuntu-ports xenial-security multiverse
+
 deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial-updates multiverse
 # deb-src http://us.ports.ubuntu.com/ubuntu-ports/ xenial-updates multiverse
 
-## N.B. software from this repository may not have been tested as
-## extensively as that contained in the main release, although it includes
-## newer versions of some applications which may provide useful features.
-## Also, please note that software in backports WILL NOT receive any review
-## or updates from the Ubuntu security team.
-deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial-backports main restricted universe multiverse
-# deb-src http://us.ports.ubuntu.com/ubuntu-ports/ xenial-backports main restricted universe multiverse
-
-## Uncomment the following two lines to add software from Canonical's
-## 'partner' repository.
-## This software is not part of Ubuntu, but is offered by Canonical and the
-## respective vendors as a service to Ubuntu users.
-# deb http://archive.canonical.com/ubuntu xenial partner
-# deb-src http://archive.canonical.com/ubuntu xenial partner
-
-deb http://ports.ubuntu.com/ubuntu-ports xenial-security main restricted
-# deb-src http://ports.ubuntu.com/ubuntu-ports xenial-security main restricted
-deb http://ports.ubuntu.com/ubuntu-ports xenial-security universe
-# deb-src http://ports.ubuntu.com/ubuntu-ports xenial-security universe
-deb http://ports.ubuntu.com/ubuntu-ports xenial-security multiverse
-# deb-src http://ports.ubuntu.com/ubuntu-ports xenial-security multiverse
+deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial-backports multiverse
+# deb-src http://us.ports.ubuntu.com/ubuntu-ports/ xenial-backports multiverse
 __EOF__
+fi
+
+# deb http://us.ports.ubuntu.com/ubuntu-ports/ xenial universe
+
+# Some packages could not be installed. This may mean that you have
+# requested an impossible situation or if you are using the unstable
+# distribution that some required packages have not yet been created
+# or been moved out of Incoming.
+# The following information may help to resolve the situation:
+# 
+# The following packages have unmet dependencies:
+#  gcc-4.7-plugin-dev : Depends: libgmpv4-dev (>= 2:5.0.1~) but it is not going to be installed
+#  lib32gcc-4.9-dev-s390x-cross : Depends: gcc-4.9-s390x-linux-gnu-base (>= 4.9.3-13ubuntu2) but it is not installable
+#  lib64gcc-4.8-dev-powerpc-cross : Depends: gcc-4.8-powerpc-linux-gnu-base (= 4.8.5-4ubuntu2cross3) but it is not installable
+#                                   Depends: lib64asan0-powerpc-cross (>= 4.8.5-4ubuntu2cross3) but it is not going to be installed
+#  lib64gcc-4.9-dev-powerpc-cross : Depends: gcc-4.9-powerpc-linux-gnu-base (>= 4.9.3-13ubuntu2) but it is not installable
+#                                   Depends: lib64asan1-powerpc-cross (>= 4.9.3-13ubuntu2cross1) but it is not going to be installed
+#  libgcc-4.7-dev-armel-cross : Depends: gcc-4.7-arm-linux-gnueabi-base (= 4.7.4-3ubuntu12cross3) but it is not installable
+#  libgcc-4.8-dev-arm64-cross : Depends: gcc-4.8-aarch64-linux-gnu-base (= 4.8.5-4ubuntu1cross2) but it is not installable
+#  libgcc-4.8-dev-powerpc-cross : Depends: gcc-4.8-powerpc-linux-gnu-base (= 4.8.5-4ubuntu2cross3) but it is not installable
+#                                 Depends: libasan0-powerpc-cross (>= 4.8.5-4ubuntu2cross3) but it is not going to be installed
+#  libgcc-4.8-dev-ppc64el-cross : Depends: gcc-4.8-powerpc64le-linux-gnu-base (= 4.8.5-4ubuntu1cross2) but it is not installable
+#  libgcc-4.9-dev-arm64-cross : Depends: gcc-4.9-aarch64-linux-gnu-base (>= 4.9.3-13ubuntu2) but it is not installable
+#  libgcc-4.9-dev-powerpc-cross : Depends: gcc-4.9-powerpc-linux-gnu-base (>= 4.9.3-13ubuntu2) but it is not installable
+#                                 Depends: libasan1-powerpc-cross (>= 4.9.3-13ubuntu2cross1) but it is not going to be installed
+#  libgcc-4.9-dev-ppc64el-cross : Depends: gcc-4.9-powerpc64le-linux-gnu-base (>= 4.9.3-13ubuntu2) but it is not installable
+#  libgcc-4.9-dev-s390x-cross : Depends: gcc-4.9-s390x-linux-gnu-base (>= 4.9.3-13ubuntu2) but it is not installable
+#  libhfgcc-4.7-dev-armel-cross : Depends: gcc-4.7-arm-linux-gnueabi-base (= 4.7.4-3ubuntu12cross3) but it is not installable
+# E: Unable to correct problems, you have held broken packages.
+
+echo
+echo "The resulting /etc/apt/sources.list"
+cat "/etc/apt/sources.list" | egrep '^deb '
+echo "---"
 
 apt-get update 
 apt-get upgrade --yes 
 
-apt-get install --yes time
+# -----------------------------------------------------------------------------
 
 apt-get clean
 apt-get autoclean
@@ -118,5 +156,8 @@ apt-get autoremove
 echo
 uname -a
 lsb_release -a
+
+echo
+echo "Container done."
 
 # -----------------------------------------------------------------------------
