c390c47cf46911362973276375cafe12e0a566c3
Liviu Ionescu <ilg@livius.net>
2020-02-04 01:53:15 +0200

    add ubuntu/12-xbb

---
+++ ubuntu/12-xbb/README.md
@@ -0,0 +1,34 @@
+
+## Build Docker images
+
+There are two scripts:
+
+- `amd64-build-v3.1.sh` -> `ilegeul/ubuntu:amd64-12.04-bootstrap-v3.1`
+- `i386-build-v3.1.sh` -> `ilegeul/ubuntu:i386-12.04-bootstrap-v3.1`
+
+```console
+$ bash ~/Downloads/xpack-build-box.git/ubuntu/12-bootstrap/amd64-build-v3.1.sh
+$ bash ~/Downloads/xpack-build-box.git/ubuntu/12-bootstrap/i386-build-v3.1.sh
+
+$ docker images
+```
+
+As a shortcut, macOS users can double click the `.command` files in Finder.
+
+## Test
+
+The test was performed on a macOS.
+
+```console
+$ docker run --interactive --tty ilegeul/ubuntu:amd64-12.04-bootstrap-v3.1
+$ docker run --interactive --tty ilegeul/ubuntu:i386-12.04-bootstrap-v3.1
+```
+
+## Publish
+
+To publish, use:
+
+```console
+$ docker push "ilegeul/ubuntu:amd64-12.04-bootstrap-v3.1"
+$ docker push "ilegeul/ubuntu:i386-12.04-bootstrap-v3.1"
+```
---
+++ ubuntu/12-xbb/container-scripts/build-v3.1.sh
@@ -0,0 +1,125 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+# Safety settings (see https://gist.github.com/ilg-ul/383869cbb01f61a51c4d).
+
+if [[ ! -z ${DEBUG} ]]
+then
+  set ${DEBUG} # Activate the expand mode if DEBUG is anything but empty.
+else
+  DEBUG=""
+fi
+
+set -o errexit # Exit if command failed.
+set -o pipefail # Exit if pipe failed.
+set -o nounset # Exit if variable not set.
+
+# Remove the initial space and instead use '\n'.
+IFS=$'\n\t'
+
+# -----------------------------------------------------------------------------
+# Identify the script location, to reach, for example, the helper scripts.
+
+script_path="$0"
+if [[ "${script_path}" != /* ]]
+then
+  # Make relative path absolute.
+  script_path="$(pwd)/$0"
+fi
+
+script_name="$(basename "${script_path}")"
+
+script_folder_path="$(dirname "${script_path}")"
+script_folder_name="$(basename "${script_folder_path}")"
+
+# =============================================================================
+# This script creates the XBB bootstrap docker images.
+
+XBB_VERSION="3.1"
+
+WORK_FOLDER_PATH="${HOME}/Work"
+
+XBB_FOLDER="/opt/xbb"
+XBB_BOOTSTRAP_FOLDER="/opt/xbb-bootstrap"
+
+IS_BOOTSTRAP="n"
+
+# -----------------------------------------------------------------------------
+
+helper_folder_path="${script_folder_path}/helper"
+
+source "${helper_folder_path}/common-functions-source.sh"
+source "${helper_folder_path}/common-docker-functions-source.sh"
+
+source "${helper_folder_path}/common-libs-functions-source.sh"
+source "${helper_folder_path}/common-apps-functions-source.sh"
+
+source "${helper_folder_path}/common-versions-source.sh"
+
+function do_cleanup()
+{
+  if [ -f "${WORK_FOLDER_PATH}/.dockerenv" ]
+  then
+    rm -rf "${WORK_FOLDER_PATH}"
+  fi
+}
+
+
+# -----------------------------------------------------------------------------
+
+detect_host
+
+docker_prepare_env
+
+prepare_xbb_env
+
+source "${XBB_BOOTSTRAP_FOLDER}/xbb-source.sh"
+
+export TEXLIVE_FOLDER="/opt/texlive"
+export XBB_BOOTSTRAP_FOLDER="/opt/xbb-bootstrap"
+
+# Adjust PATH to prefer the XBB bootstrap binaries.
+function xbb_activate_bootstrap()
+{
+  PATH="${XBB_BOOTSTRAP_FOLDER}/bin:${PATH}"
+
+  export PATH
+}
+
+# Add TeX to PATH.
+function xbb_activate_tex()
+{
+  PATH="${TEXLIVE_FOLDER}/bin/x86_64-ubuntu:${PATH}"
+
+  export PATH
+}
+
+# Override function, must be here.
+function xbb_activate()
+{
+  xbb_activate_bootstrap      # Use only bootstrap binaries, not xbb
+  xbb_activate_installed_dev  # Use xbb libraries and headers
+}
+
+create_xbb_source
+
+echo
+echo "$(uname) XBB build script started..."
+
+# -----------------------------------------------------------------------------
+
+do_build_versions
+
+do_strip_debug_libs
+
+# -----------------------------------------------------------------------------
+
+echo
+echo "$(uname) XBB created in \"${INSTALL_FOLDER_PATH}\""
+
+do_cleanup
+
+echo
+echo "Container done."
+
+# -----------------------------------------------------------------------------
