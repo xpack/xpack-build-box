a50382e1c0284163bed12ec0eaa6a5911b806b22
Liviu Ionescu <ilg@livius.net>
2020-02-07 17:33:49 +0200

    helper/common-apps: bison with -lrt only for linux

--- helper/common-apps-functions-source.sh
+++ helper/common-apps-functions-source.sh
@@ -2167,8 +2167,12 @@ function do_bison()
       export CFLAGS="${XBB_CFLAGS}"
       export CXXFLAGS="${XBB_CXXFLAGS}"
       export LDFLAGS="${XBB_LDFLAGS_APP}"
-      # undefined reference to `clock_gettime' on docker
-      export LIBS="-lrt"
+
+      if [ "${HOST_UNAME}" == "Linux" ]
+      then
+        # undefined reference to `clock_gettime' on docker
+        export LIBS="-lrt"
+      fi
 
       if [ ! -f "config.status" ]
       then
