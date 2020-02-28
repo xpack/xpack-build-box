45f9b4e5424ed7e1d2432c4054562802658502e9
Liviu Ionescu <ilg@livius.net>
2020-02-28 02:35:16 +0200

    ubuntu/12-updated: use common docker_replace_source_list

--- ubuntu/12-updated/container-scripts/build-v3.1.sh
+++ ubuntu/12-updated/container-scripts/build-v3.1.sh
@@ -34,6 +34,12 @@ script_folder_name="$(basename "${script_folder_path}")"
 
 # =============================================================================
 
+helper_folder_path="${script_folder_path}/helper"
+
+source "${helper_folder_path}/common-docker-functions-source.sh"
+
+# -----------------------------------------------------------------------------
+
 env
 unset TERM
 
@@ -53,85 +59,7 @@ ln -s /bin/true /sbin/initctl
 
 # -----------------------------------------------------------------------------
 
-echo
-echo "The orginal /etc/apt/sources.list"
-cat "/etc/apt/sources.list"
-echo "---"
-
-# -----------------------------------------------------------------------------
-
-# Use http://old-releases.ubuntu.com/ubuntu
-
-echo "Creating new sources.list..."
-# Note: __EOF__ is quoted to prevent substitutions here.
-cat <<'__EOF__'  >"/etc/apt/sources.list"
-# https://help.ubuntu.com/community/Repositories/Ubuntu
-# See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to
-# newer versions of the distribution.
-
-deb http://old-releases.ubuntu.com/ubuntu precise main restricted
-# deb-src http://old-releases.ubuntu.com/ubuntu precise main restricted
-deb http://old-releases.ubuntu.com/ubuntu precise-security main restricted
-# deb-src http://old-releases.ubuntu.com/ubuntu precise-security main restricted
-
-## Major bug fix updates produced after the final release of the
-## distribution.
-deb http://old-releases.ubuntu.com/ubuntu precise-updates main restricted
-# deb-src http://old-releases.ubuntu.com/ubuntu precise-updates main restricted
-
-deb http://old-releases.ubuntu.com/ubuntu precise-backports main restricted 
-# deb-src http://old-releases.ubuntu.com/ubuntu precise-backports main restricted 
-__EOF__
-
-if false
-then
-cat <<'__EOF__' >>"/etc/apt/sources.list"
-
-## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
-## team. Also, please note that software in universe WILL NOT receive any
-## review or updates from the Ubuntu security team.
-deb http://old-releases.ubuntu.com/ubuntu precise universe
-# deb-src http://old-releases.ubuntu.com/ubuntu precise universe
-deb http://old-releases.ubuntu.com/ubuntu precise-security universe
-# deb-src http://old-releases.ubuntu.com/ubuntu precise-security universe
-
-deb http://old-releases.ubuntu.com/ubuntu precise-updates universe
-# deb-src http://old-releases.ubuntu.com/ubuntu precise-updates universe
-
-deb http://old-releases.ubuntu.com/ubuntu precise-backports universe 
-# deb-src http://old-releases.ubuntu.com/ubuntu precise-backports universe 
-__EOF__
-fi
-
-if false
-then
-cat <<'__EOF__' >>"/etc/apt/sources.list"
-
-## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu 
-## team, and may not be under a free licence. Please satisfy yourself as to 
-## your rights to use the software. Also, please note that software in 
-## multiverse WILL NOT receive any review or updates from the Ubuntu
-## security team.
-deb http://old-releases.ubuntu.com/ubuntu precise multiverse
-# deb-src http://old-releases.ubuntu.com/ubuntu precise multiverse
-deb http://old-releases.ubuntu.com/ubuntu precise-security multiverse
-# deb-src http://old-releases.ubuntu.com/ubuntu precise-security multiverse
-
-deb http://old-releases.ubuntu.com/ubuntu precise-updates multiverse
-# deb-src http://old-releases.ubuntu.com/ubuntu precise-updates multiverse
-
-deb http://old-releases.ubuntu.com/ubuntu precise-backports multiverse
-# deb-src http://old-releases.ubuntu.com/ubuntu precise-backports multiverse
-__EOF__
-fi
-
-echo
-echo "The resulting /etc/apt/sources.list"
-cat "/etc/apt/sources.list" | egrep '^deb '
-echo "---"
-
-apt-get update 
-apt-get upgrade --yes 
+docker_replace_source_list "http://old-releases.ubuntu.com/ubuntu/" "precise"
 
 # -----------------------------------------------------------------------------
 
