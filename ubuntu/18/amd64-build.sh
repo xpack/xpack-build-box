6a452ab0d8a8b3a896eb914f3f725c92f5ec9907
Liviu Ionescu <ilg@livius.net>
2020-03-01 22:34:29 +0200

    add amd64 & i386 scripts to all ubuntu

---
+++ ubuntu/18/amd64-build.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+# Safety settings (see https://gist.github.com/ilg-ul/383869cbb01f61a51c4d).
+
+if [[ ! -z ${DEBUG} ]]
+then
+  set ${DEBUG} # Activate the expand mode if DEBUG is anything but empty.
+else
+  DEBUG=""
+fi
+
+set -o errexit # Exit if command failed.
+set -o pipefail # Exit if pipe failed.
+set -o nounset # Exit if variable not set.
+
+# Remove the initial space and instead use '\n'.
+IFS=$'\n\t'
+
+# -----------------------------------------------------------------------------
+# Identify the script location, to reach, for example, the helper scripts.
+
+script_path="$0"
+if [[ "${script_path}" != /* ]]
+then
+  # Make relative path absolute.
+  script_path="$(pwd)/$0"
+fi
+
+script_name="$(basename "${script_path}")"
+
+script_folder_path="$(dirname "${script_path}")"
+script_folder_name="$(basename "${script_folder_path}")"
+
+# =============================================================================
+
+# Walk two steps up.
+helper_folder_path="$(dirname $(dirname "${script_folder_path}"))/helper"
+
+source "${helper_folder_path}/common-functions-source.sh"
+source "${helper_folder_path}/common-docker-functions-source.sh"
+
+# -----------------------------------------------------------------------------
+
+host_init_docker_env
+host_init_docker_input
+
+arch="amd64"
+
+docker_build_from_archive "${arch}" "${arch}-ubu18-rootfs.xz" "ilegeul/ubuntu:${arch}-18.04"
+
+host_clean_docker_input
+
+echo 
+echo "Done."
+
+# -----------------------------------------------------------------------------
