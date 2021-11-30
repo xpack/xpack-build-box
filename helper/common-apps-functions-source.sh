81b6cadc64740f8fe9ee5be67cf7cd75042aa1e2
Liviu Ionescu <ilg@livius.net>
2021-11-30 11:07:16 +0200

    common-apps: bump homebrew to 20211128

--- helper/common-apps-functions-source.sh
+++ helper/common-apps-functions-source.sh
@@ -486,9 +486,9 @@ function build_native_gcc()
   elif is_darwin && [ "${native_gcc_version}" == "11.2.0" ]
   then
     # https://github.com/fxcoudert/gcc/archive/refs/tags/gcc-11.2.0-arm-20211126.tar.gz
-    local native_gcc_archive="gcc-11.2.0-arm-20211126.tar.gz"
+    local native_gcc_archive="gcc-11.2.0-arm-20211128.tar.gz"
     local native_gcc_url="https://github.com/fxcoudert/gcc/archive/refs/tags/${native_gcc_archive}"
-    local native_gcc_src_folder_name="gcc-gcc-11.2.0-arm-20211126"
+    local native_gcc_src_folder_name="gcc-gcc-11.2.0-arm-20211128"
   elif is_darwin && [ "${native_gcc_version}" == "11.1.0" ]
   then
     # https://github.com/fxcoudert/gcc/archive/refs/tags/gcc-11.1.0-arm-20210504.tar.gz
